4acb17cfb8ee53fc78e27b36a5d531ec3e07a91d
Macbook Po <Mac@Macbooks-MacBook-Pro.local>
2017-06-05 21:26:59 +0500

    push bakery project

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->enum('type', ['store_manager', 'kitchen_manager', 'bakery_manager']);
+            $table->string('username', 100)->unique();
+            $table->string('email', 100);
+            $table->string('phone_no', 100);
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
